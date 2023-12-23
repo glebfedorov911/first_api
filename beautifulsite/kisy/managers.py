7d812a72bac08c1041033014fdb16a7c15bed2f5
Gleb <glebobas111@vk.com>
2023-12-23 15:06:59 +0300

    Update managers.py

--- beautifulsite/kisy/managers.py
+++ beautifulsite/kisy/managers.py
@@ -29,6 +29,7 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault('is_active', True)
         return self._create_user(email=email, password=password, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
